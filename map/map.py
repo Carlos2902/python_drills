705f5edb3544ae789bcf111dbbe0e9e5b6490635
Carlos Lopez <carloslopezr29@gmail.com>
2024-04-08 11:30:06 -0400

    Map method

---
+++ map/map.py
@@ -0,0 +1,11 @@
+menu = ['express','capuccino', 'chinito', 'americano', 'cortado']
+
+def get_coffee_c(coffee):
+    if coffee[0] == 'c':
+        return coffee
+    
+
+my_map_list = map(get_coffee_c, menu)
+
+for item in my_map_list:
+    print(item)
