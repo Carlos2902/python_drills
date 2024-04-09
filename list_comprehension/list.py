0b78e010977add456955d25301f749eed399fd69
Carlos Lopez <carloslopezr29@gmail.com>
2024-04-09 09:06:15 -0400

    Set comprehension

--- list_comprehension/list.py
+++ list_comprehension/list.py
@@ -24,17 +24,21 @@
 #{key:value, for (key,value) in <sequence> <conditional>}
 
 #1. Using range
-my_dict_range = {x:x*2 for x in range(20)}
-print('Using range(): ',my_dict_range )
+# my_dict_range = {x:x*2 for x in range(20)}
+# print('Using range(): ',my_dict_range )
 
 #delcaring two lists
-months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
-number = [1,2,3,4,5,6,7,8,9,10,11,12]
+# months = ["Jan", "Feb", "Mar", "Apr", "May", "June", "July", "Aug", "Sept", "Oct", "Nov", "Dec"]
+# number = [1,2,3,4,5,6,7,8,9,10,11,12]
 
 #Dictionary for one list:
-number_dictionary = {x:x for x in number}
-print('Dictionary of numbers:', number_dictionary)
+# number_dictionary = {x:x for x in number}
+# print('Dictionary of numbers:', number_dictionary)
 
 #Dictionary with two lists:
-two_lists = {key:value for (key,value) in zip (number, months)}
-print('Two dictionary list: ', two_lists)
+# two_lists = {key:value for (key,value) in zip (number, months)}
+# print('Two dictionary list: ', two_lists)
+
+#now for sets, the structure of the comprehension is the same, but instead of [] we use {}
+my_new_set = {x for x in range(20) if x not in [10,15,20]}
+print('My new set: ', my_new_set)
