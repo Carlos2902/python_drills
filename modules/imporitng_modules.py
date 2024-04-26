ceaa4d61e4a21aa6eb4100453ea014217702ba50
Carlos Lopez <carloslopezr29@gmail.com>
2024-04-26 15:53:27 -0400

    giving alias

---
+++ modules/imporitng_modules.py
@@ -0,0 +1,25 @@
+# import math
+
+# root = math.sqrt(210)
+
+# print(root)
+
+
+
+#importing only specific functions of a given module
+# from math import sqrt
+# root = sqrt(9)
+
+# print(root)
+
+
+#givin alias
+from math import sqrt as sq
+
+root = sq(10)
+print(root)
+
+
+import math as m
+cosine = m.cos(202)
+print(cosine)
