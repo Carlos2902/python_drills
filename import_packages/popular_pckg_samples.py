605ab1f7ae70c34b99ac311b39f71f46285ec37c
Carlos Lopez <carloslopezr29@gmail.com>
2024-04-29 11:03:16 -0400

    Testing PiPy Packages

--- import_packages/popular_pckg_samples.py
+++ import_packages/popular_pckg_samples.py
@@ -1,10 +1,22 @@
 import nltk
-nltk.download('punkt')
 from nltk.tokenize import word_tokenize
 from nltk.corpus import stopwords
 
 text = "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book."
 
-
+print("\n")
 #using the first function from the nltk package
 print(word_tokenize(text))
+print("\n")
+#function that separates the text by sentences.
+print(nltk.tokenize.sent_tokenize(text))
+
+print("\n")
+stopwords = stopwords.words("english")
+new_text=[]
+
+for i in text.split():
+    if i not in stopwords:
+        new_text.append(i)
+# Print statement 3
+print(new_text)
