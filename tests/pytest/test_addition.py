3a6297935bea7611f0dba66f8383d7561d54b075
Carlos Lopez <carloslopezr29@gmail.com>
2024-05-03 13:39:39 -0400

    testing the pytest flags

--- tests/pytest/test_addition.py
+++
@@ -1,7 +0,0 @@
-import addition
-import pytest
-
-def test_add():
-    assert addition.add(4,9) == 13
-def test_sub():
-    assert addition.sub(4,9) == -5
---
+++ tests/test_addition.py
@@ -0,0 +1,7 @@
+import addition
+# import pytest
+
+def test_add():
+    assert addition.add(4,9) == 1
+def test_sub():
+    assert addition.sub(4,9) == -5
