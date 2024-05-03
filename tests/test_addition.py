eda9d78486d9764373ecd62ad908085116b5b3ce
Carlos Lopez <carloslopezr29@gmail.com>
2024-05-03 13:50:46 -0400

    Using marks for tests

--- tests/test_addition.py
+++ tests/test_addition.py
@@ -1,7 +1,13 @@
 import addition
-# import pytest
+import pytest
 
+
+
+
+@pytest.mark.sum1
 def test_add():
-    assert addition.add(4,9) == 1
+    assert addition.add(4,9) == 13
+
+    
 def test_sub():
     assert addition.sub(4,9) == -5
